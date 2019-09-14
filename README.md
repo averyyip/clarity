43fa0d635a992d54df794b41426e59edb5a6535a
Avery <averyyip@berkeley.edu>
2019-09-14 12:38:47 -0700

    Added videojs dependency

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-#To Run
+# To Run
 1. Install node
 2. `npm install express`
 3. `node server.js`
--- server.js
+++ server.js
@@ -13,6 +13,9 @@ app.get('/', function (req, res) {
   res.render('index');
 })
 
+app.get('/lecture_playback', function (req, res) {
+  res.render('lecture_playback');
+})
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
